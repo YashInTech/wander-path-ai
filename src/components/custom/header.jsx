f7cc205c7d851718349b5f894bcbd67043340fbb
YashInTech <YashInTech@users.noreply.github.com>
2024-12-26 23:52:04 +0530

    Landing Page Created

--- src/components/custom/header.jsx
+++ src/components/custom/header.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='p-3 shadow-md flex justify-between items-center px-5'>
+        <img src="/mainLogo.png" />
+        <div>
+            <Button>Sign In</Button>
+        </div>
+    </div>
   )
 }
 
--- src/components/custom/hero.jsx
+++ src/components/custom/hero.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className='flex flex-col items-center mx-56 gap-9'>
+        <h1 className='font-extrabold text-[45px] text-center mt-16'>
+        <span className='text-[#080c52]'>Discover Your Next Adventure with AI: </span>
+        <span className='text-[#00a5bd]'>Personalized Itineraries at Your Fingertips</span></h1>
+        <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom itineraries tailored to your interests tailored to your interests and budget.</p>
+
+        <Button>Get Started</Button>
+    
+    </div>
   )
 }
 
